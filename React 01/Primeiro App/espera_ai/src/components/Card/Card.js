7366db2d3f25ab768b202faa7c5a94eaa00648c0
KEILA DAMASCENO BARRETO <keila_barreto@estudante.sesisenai.org.br>
2024-05-16 19:22:42 -0300

    primeiro app

---
+++ React 01/Primeiro App/espera_ai/src/components/Card/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: #dddae2;
+    width: 400px;
+    border-radius: 20px;
+    padding: 20px;
+    margin: 20px;
+
+    h1 {
+        color: blue;
+    }
+`
+
+export function Card({title, content}){
+    return (
+        <Container>
+            <h1>{ title }</h1>
+            <p>{content}</p>
+        </Container>
+    );
+}
